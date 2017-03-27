fc10d9a77bf8c3994036e319d0e07e301e8c3851
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2017-03-27 14:12:45 +0300

    added media queries

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $('.navbar-toggler').on('click', function(){
 	var target = $(this).attr('href');
-	$(this).toggleClass('active');
+	$(this).toggleClass('active');	
 	$(target).toggleClass('collapse');
 })
 });
