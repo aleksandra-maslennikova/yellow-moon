5139e57c8cdaa8ed45c21b07082882fd7335412a
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2017-04-05 20:37:15 +0300

    fixed toggle, deleted images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-$('.navbar-toggler').on('click', function(){
+$('.navbar-toggler').on('click', function(e){
+	e.preventDefault();
 	var target = $(this).attr('href');
 	$(this).toggleClass('active');	
 	$(target).toggleClass('collapse');
